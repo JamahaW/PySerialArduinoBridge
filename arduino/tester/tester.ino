5a3b19e6b7e66dc6823bc904e00fd3bf18891528
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-09-10 00:27:28 +0300

    wok

--- arduino/tester/tester.ino
+++ arduino/tester/tester.ino
@@ -5,12 +5,54 @@ enum Commands : byte {
   DELAY_MS = 0x13,
 };
 
-
-
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  pinMode(13, OUTPUT);
+}
+
+void wait_bytes(uint8_t b) {
+  while (Serial.available() < b) {
+    delay(1);
+  }
+}
+
+void on_pin_mode() {
+  wait_bytes(2);
+  uint8_t pin = Serial.read();
+  uint8_t mode = Serial.read();
+  pinMode(pin, mode);
+}
+
+void on_di_write() {
+  wait_bytes(2);
+  uint8_t pin = Serial.read();
+  bool state = Serial.read();
+  digitalWrite(pin, state);
+}
+
+void on_DIGITAL_READ() {
+  wait_bytes(2);
+  uint8_t pin = Serial.read();
+  uint8_t state = digitalRead(pin);
+  Serial.write(state);
+}
+
+void on_DELAY_MS () {
+  wait_bytes(4);
+  uint32_t t;
+  Serial.readBytes((uint8_t*)&t, sizeof(t));
 }
 
 void loop() {
+  if (Serial.available() < 1) return;
 
+  uint8_t code = Serial.read();
+
+  switch (code) {
+    case PIN_MODE: on_pin_mode(); break;
+    case DIGITAL_WRITE: on_di_write(); break;
+    case DIGITAL_READ: on_DIGITAL_READ(); break;
+    case DELAY_MS: on_DELAY_MS(); break;
+  }
 }
--- src/main.py
+++ src/main.py
@@ -1,5 +1,54 @@
+from struct import Struct
+from time import sleep
+
+from serial import Serial
+
+INPUT = 0x0
+OUTPUT = 0x1
+INPUT_PULLUP = 0x2
+
+
+class Arduino:
+    __PIN_MODE = 0x10
+    __DIGITAL_WRITE = 0x11
+    __DIGITAL_READ = 0x12
+    __DELAY_MS = 0x13
+
+    def __init__(self, serial: Serial) -> None:
+        self.serial = serial
+
+        self.__bool = Struct("?")
+        self.__u8 = Struct("B")
+        self.__u32 = Struct("L")
+
+    def pinMode(self, pin: int, mode: int) -> None:
+        self.serial.write(self.__u8.pack(self.__PIN_MODE) + self.__u8.pack(pin) + self.__u8.pack(mode))
+
+    def digitalWrite(self, pin: int, state: bool) -> None:
+        self.serial.write(self.__u8.pack(self.__DIGITAL_WRITE) + self.__u8.pack(pin) + self.__bool.pack(state))
+
+    def digitalRead(self, pin: int) -> bool:
+        pass
+
+    def delay(self, milliseconds: int) -> None:
+        self.serial.write(self.__u8.pack(self.__DELAY_MS) + self.__u32.pack(milliseconds))
+        sleep(0.001 * milliseconds)
+
+
 def main() -> None:
-    return
+    arduino = Arduino(Serial("COM10", 115200))
+
+    sleep(0.5)
+
+    arduino.pinMode(13, OUTPUT)
+
+    for i in range(10):
+        arduino.digitalWrite(13, True)
+        arduino.delay(100)
+        arduino.digitalWrite(13, False)
+        arduino.delay(1000)
+
+    arduino.serial.close()
 
 
 if __name__ == '__main__':
