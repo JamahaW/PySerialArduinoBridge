b19fa8eacb70480bc0e8596b06b368bddac1c5ed
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-09-10 01:33:08 +0300

    update tester.ino

--- arduino/tester/tester.ino
+++ arduino/tester/tester.ino
@@ -1,4 +1,5 @@
-enum Commands : byte {
+// Перечисление со всеми командами
+enum Commands : uint8_t {
   PIN_MODE = 0x10,
   DIGITAL_WRITE = 0x11,
   DIGITAL_READ = 0x12,
@@ -6,6 +7,7 @@ enum Commands : byte {
 };
 
 void setup() {
+  // повышенная скорость порта
   Serial.begin(115200);
 
   pinMode(13, OUTPUT);
@@ -13,40 +15,48 @@ void setup() {
 
 void wait_bytes(uint8_t b) {
   while (Serial.available() < b) {
-    delay(1);
+    ; // Ожидание прибытия всего пакета
   }
 }
 
 void on_pin_mode() {
-  wait_bytes(2);
+  wait_bytes(sizeof(uint8_t) + sizeof(uint8_t));
+
   uint8_t pin = Serial.read();
   uint8_t mode = Serial.read();
+
   pinMode(pin, mode);
 }
 
 void on_di_write() {
-  wait_bytes(2);
+  wait_bytes(sizeof(uint8_t) + sizeof(bool));
+
   uint8_t pin = Serial.read();
   bool state = Serial.read();
+
   digitalWrite(pin, state);
 }
 
 void on_DIGITAL_READ() {
-  wait_bytes(1);
+  wait_bytes(sizeof(uint8_t));
+
   uint8_t pin = Serial.read();
+
   uint8_t state = digitalRead(pin);
   Serial.write(state);
 }
 
 void on_DELAY_MS () {
-  wait_bytes(4);
+  wait_bytes(sizeof(uint32_t);
+
   uint32_t t;
   Serial.readBytes((uint8_t*)&t, sizeof(t));
+
+  delay(t);
 }
 
 void loop() {
   if (Serial.available() < 1) return;
-
   uint8_t code = Serial.read();
 
   switch (code) {
