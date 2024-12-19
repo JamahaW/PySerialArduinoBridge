73e7c7c89d126356badda1d770ecd1701a39f99e
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-12-19 22:39:30 +0300

    move to arduino.py

--- desktop/python/src/main.py
+++ desktop/python/src/main.py
@@ -1,31 +1,6 @@
-from serialcmd.core.errorenum import ErrorEnum
-from serialcmd.protocol import Protocol
-
-
-class ArduinoError(ErrorEnum):
-    ok = 0x00
-    fail = 0x01
-
-
-class ArduinoProtocol(Protocol[ArduinoError]):
-    """Пример подключения к Arduino с минимальным набором команд"""
-
+def _launch() -> None:
     pass
 
 
-def main() -> None:
-    INPUT = 0x0
-    OUTPUT = 0x1
-    INPUT_PULLUP = 0x2
-    LED_BUILTIN = 13
-
-    # Подключение к Arduino, скорость повышенная
-    # arduino = ArduinoProtocol(Serial("COM10", 115200))
-
-    # Ожидание пока вся инициализация slave платы пройдёт ...
-
-    # Закрытие порта
-
-
 if __name__ == '__main__':
-    main()
+    _launch()
