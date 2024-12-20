bf66632aba9704a71189c0182ce7fb858e6d9143
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-12-20 20:16:54 +0300

    upd

--- desktop/python/src/main.py
+++ desktop/python/src/main.py
@@ -1,3 +1,6 @@
+import timeit
+from typing import Callable
+
 from arduino import ArduinoProtocol
 from arduino import LED_BUILTIN
 from arduino import OUTPUT
@@ -13,7 +16,7 @@ def _launch() -> str:
 
     arduino = ArduinoProtocol(Serial(ports[0], 115200))
 
-    print("\n".join(map(str, arduino.getCommands())))
+    # print("\n".join(map(str, arduino.getCommands())))
 
     startup = arduino.begin()
 
@@ -22,11 +25,27 @@ def _launch() -> str:
 
     print(f"Пакет ответа инициализации ведомого устройства: {startup=}")
 
+    #
+
     arduino.pinMode(LED_BUILTIN, OUTPUT)
 
-    for i in range(20):
-        arduino.digitalWrite(LED_BUILTIN, i % 2 == 0)
-        arduino.delay(500)
+    def _blink():
+        arduino.digitalWrite(LED_BUILTIN, True)
+        arduino.digitalWrite(LED_BUILTIN, False)
+
+    def _test():
+        # arduino.delay(500)
+
+        print(arduino.millis())
+
+        return
+
+    def calc(f: Callable[[], None], n: int = 1000) -> float:
+        return timeit.timeit(f, number=n) / n
+
+    # t = calc(_blink)
+    t = calc(_test, 10)
+    print(f"{t * 1000.0} ms ({1 / t})")
 
     return "Успешное завершение"
 
